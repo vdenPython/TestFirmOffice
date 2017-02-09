a5fa54bdcf4ca6cabbf137592521db144440aa7e
vden <vden@hmgaz.ru>
2017-02-09 16:34:51 +0500

    Начальный тест добавления группы

--- test_add_group.py
+++ test_add_group.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-#from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.action_chains import ActionChains
 import time, unittest
@@ -14,22 +13,26 @@ def is_alert_present(wd):
 class test_add_group(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver('/home/vden/PycharmProjects/ChromeDriver/chromedriver')
-        #self.wd = WebDriver('/home/vden/PycharmProjects/geckodriver')
         self.wd.implicitly_wait(60)
     
     def test_test_add_group(self):
         success = True
         wd = self.wd
         wd.get("http://127.0.0.1:8000/admin/")
-        wd.find_element_by_id("id_password").click()
-        wd.find_element_by_id("id_password").send_keys("\\undefined")
-        wd.find_element_by_id("id_username").click()
+
         wd.find_element_by_id("id_username").clear()
         wd.find_element_by_id("id_username").send_keys("test")
-        wd.find_element_by_id("id_password").click()
         wd.find_element_by_id("id_password").clear()
         wd.find_element_by_id("id_password").send_keys("test12345")
         wd.find_element_by_xpath("//div[@class='submit-row']/input").click()
+
+        wd.find_element_by_link_text("Группы").click()
+        wd.find_element_by_link_text("ДОБАВИТЬ ГРУППА").click()
+        wd.find_element_by_id("id_name").clear()
+        wd.find_element_by_id("id_name").send_keys("test1")
+        wd.find_element_by_name("_save").click()
+
+        wd.find_element_by_link_text("Администрирование Django").click()
         wd.find_element_by_link_text("ВЫЙТИ").click()
         self.assertTrue(success)
     
